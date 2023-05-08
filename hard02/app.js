6d817cedb0f8c32988f8342faf75296ad79d4d83
Михаил <101520011+MikhaiLS9@users.noreply.github.com>
2023-05-08 23:46:58 +0300

    Delete hard02 directory

--- hard02/app.js
+++
@@ -1,9 +0,0 @@
-const num = 266219 
-const numArrStr = String(num).split('')
-const numArr = numArrStr.map(item => Number(item))
-const numSum = numArr.reduce((acc,item) => acc += item ** 3)
-const numStr = String(numSum).slice(0,2)
-console.log(numStr);
-
-
-
